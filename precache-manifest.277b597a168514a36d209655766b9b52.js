71b0ddb4a336f0344970d9cf4e7e994d52e2b437
huiga <eddiex2205@gmail.com>
2021-02-21 16:34:37 -0500

    Updates

---
+++ precache-manifest.277b597a168514a36d209655766b9b52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83c4b5c9026b9c7566a1a6ee6417b9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f23f20eefe0d6f7b23",
+    "url": "/static/css/main.bb2f4458.chunk.css"
+  },
+  {
+    "revision": "97a922ffc32b25e2b734",
+    "url": "/static/js/2.08e5407a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.08e5407a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f23f20eefe0d6f7b23",
+    "url": "/static/js/main.638d2c2e.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "b3884b0e060617854c193f27d7306f2e",
+    "url": "/static/media/eddiexu.b3884b0e.png"
+  },
+  {
+    "revision": "333c0bb2a913aae1fc9736f6614ea70a",
+    "url": "/static/media/resume.333c0bb2.pdf"
+  }
+]);
