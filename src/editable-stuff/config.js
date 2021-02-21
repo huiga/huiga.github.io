df987e76039180b057fa8fc82a878332b32172c2
huiga <eddiex2205@gmail.com>
2021-02-21 14:32:17 -0500

    updating readme

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -66,45 +66,28 @@ const repos = {
 };
 
 // Leadership SECTION
-const leadership = {
-  show: false,
-  heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/eddiexu.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/eddiexu.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
-  imageSize: {
-    width:"615",
-    height:"450"
-  }
-};
-
-// SKILLS SECTION
-// const skills = {
-//   show: true,
-//   heading: "Skills",
-//   hardSkills: [
-//     { name: "Python", value: 90 },
-//     { name: "SQL", value: 75 },
-//     { name: "Data Structures", value: 85 },
-//     { name: "C/C++", value: 65 },
-//     { name: "JavaScript", value: 90 },
-//     { name: "React", value: 65 },
-//     { name: "HTML/CSS", value: 55 },
-//     { name: "C#", value: 80 },
+// const leadership = {
+//   show: false,
+//   heading: "Leadership",
+//   message:
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
+//   images: [
+//     { 
+//       img: require("../editable-stuff/eddiexu.png"), 
+//       label: "First slide label", 
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+//     },
+//     { 
+//       img: require("../editable-stuff/eddiexu.png"), 
+//       label: "Second slide label", 
+//       paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
+//     },
 //   ],
+//   imageSize: {
+//     width:"615",
+//     height:"450"
+//   }
 // };
-// Java, Python, C, HTML/CSS, JavaScript, MySQL, OCaml, MIPS, LaTex, Swift, XML, Gdscript, Octave
 
 const skills = {
   show: true,
@@ -127,9 +110,6 @@ const skills = {
 };
 
 
-
-
-// GET IN TOUCH SECTION
 const getInTouch = {
   show: true,
   heading: "Get In Touch",
@@ -138,26 +118,23 @@ const getInTouch = {
   email: "eddiexudev@gmail.com",
 };
 
-const experiences = {
-  show: false,
-  heading: "Experiences",
-  data: [
-    {
-      role: 'Software Engineer',// Here Add Company Name
-      companylogo: require('../assets/img/dell.png'),
-      date: 'June 2018 – Present',
-    },
-    {
-      role: 'Front-End Developer',
-      companylogo: require('../assets/img/boeing.png'),
-      date: 'May 2017 – May 2018',
-    },
-  ]
-}
-
-// Blog SECTION
-// const blog = {
+// const experiences = {
 //   show: false,
-// };
+//   heading: "Experiences",
+//   data: [
+//     {
+//       role: 'Software Engineer',
+//       companylogo: require('../assets/img/dell.png'),
+//       date: 'June 2018 – Present',
+//     },
+//     {
+//       role: 'Front-End Developer',
+//       companylogo: require('../assets/img/boeing.png'),
+//       date: 'May 2017 – May 2018',
+//     },
+//   ]
+// }
 
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+
+// export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export { navBar, mainBody, about, repos, skills, getInTouch };
