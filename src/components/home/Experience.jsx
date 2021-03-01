3d2395aa332e99402ba1f06b8081ba19413f01ad
huiga <eddiex2205@gmail.com>
2021-03-01 18:43:41 -0500

    experience added

--- src/components/home/Experience.jsx
+++ src/components/home/Experience.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ExperienceCard from "./ExperienceCard";
+import Card from "react-bootstrap/Card";
 import {
     Container,
     Row,
@@ -7,24 +8,48 @@ import {
 } from "react-bootstrap";
 
 const Experience = ({experiences}) => {
-    return (  
-        <section className="section">
-            <Container>
-                <Jumbotron fluid className="bg-white">
-                    <h2 className="display-4 mb-5 text-center">
-                        {experiences.heading}
-                    </h2>
-                    <Row>
+    return (
+        <Jumbotron fluid className="bg-white m-0 p-0">
+            <Card className="card shadow-sm p-3 m-0 bg-white rounded">
+            <Container fluid className="p-4" style={{width:'60%'}}>
+                <h2 className="display-4 pb-5 text-center">Work Experience</h2>
+                <div class="row">
+                    <div class="col-xl-12">
                         {
-                            experiences.data.map(data => {
-                                return <ExperienceCard key={data.company} data={data} />
-                            })
+                        experiences.data.map(data => {
+                            return <ExperienceCard key={data.role.location} data={data} />
+                        })
                         }
-                    </Row>
-                </Jumbotron>
-            </Container>
-        </section>
+                    </div>
+                </div>
+        </Container>
+        </Card>
+        </Jumbotron>
     );
 }
  
 export default Experience;
+
+
+
+
+
+
+// return (  
+//     <section className="section">
+//         <Container>
+//             <Jumbotron fluid className="bg-white">
+//                 <h2 className="display-4 mb-5 text-center">
+//                     {experiences.heading}
+//                 </h2>
+//                 <Row>
+//                     {
+//                         experiences.data.map(data => {
+//                             return <ExperienceCard key={data.company} data={data} />
+//                         })
+//                     }
+//                 </Row>
+//             </Jumbotron>
+//         </Container>
+//     </section>
+// );
--- src/components/home/ExperienceCard.jsx
+++ src/components/home/ExperienceCard.jsx
@@ -6,17 +6,25 @@ import {
  } from "react-bootstrap";
 const ExperienceCard = ({data}) => {
     return ( 
-        <Col lg="6">
-            <div className="pb-5 text-center">
-                <img className=" bg-white mb-3" src={data.companylogo} alt=""/>
-                <p className="lead">
-                    {data.role}
-                    <br/>
-                    {data.date}
-                </p>
+        <ul class="experience-tl">
+            <li>
+                <h2 className="lead">{data.date}</h2>
+                <h3 className="lead">{data.role}</h3>
+                <h4 className="lead">{data.location}</h4>
+                <h5 className="lead">{data.description}</h5>
+            </li>
+        </ul>
+        // <Col lg="6">
+        //     <div className="pb-5 text-center">
+        //         <img className=" bg-white mb-3" src={data.companylogo} alt=""/>
+        //         <p className="lead">
+        //             {data.role}
+        //             <br/>
+        //             {data.date}
+        //         </p>
                 
-            </div>
-        </Col>
+        //     </div>
+        // </Col>
      );
 }
  
